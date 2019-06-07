9efef5fa2adcbfe67134c2e00f715c57892ff9b1
YaminSAIC <yli@saicusa.com>
2019-06-07 13:23:47 -0700

    pytorch dataset builder add saic

--- second/pytorch/builder/input_reader_builder.py
+++ second/pytorch/builder/input_reader_builder.py
@@ -50,7 +50,8 @@ def build(input_reader_config,
           model_config,
           training,
           voxel_generator,
-          target_assigner=None) -> DatasetWrapper:
+          target_assigner=None,
+          dataset='kitti') -> DatasetWrapper:
     """Builds a tensor dictionary based on the InputReader config.
 
     Args:
@@ -67,6 +68,7 @@ def build(input_reader_config,
         raise ValueError('input_reader_config not of type '
                          'input_reader_pb2.InputReader.')
     dataset = dataset_builder.build(input_reader_config, model_config,
-                                    training, voxel_generator, target_assigner)
+                                    training, voxel_generator, target_assigner,
+                                    dataset=dataset)
     dataset = DatasetWrapper(dataset)
     return dataset
